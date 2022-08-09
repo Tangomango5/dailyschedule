b00eb85c6102fca254178b6a2aab3a222b109c3a
Tango Mango <aliciacastagnino@comcast.net>
2022-08-08 21:41:16 -0400

    fixed css with bootstrap and added a new function to java

---
+++ Assets/README.md
@@ -0,0 +1,5 @@
+Welcome to my daily planner!
+
+I managed to get the css to work and made the time blocks with bootstrap. 
+I also got the date to update using momentjs but I struggled to get the function that changes the colors to the past present and future.
+I will try to improve this in the future.
--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,41 @@
+//Current Date tracker
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+ 
+//variable that updates the planner every 5 minutes
+var checkTime = function () {
+
+    //Get Current time
+    var currentTime = moment().format('H');
+
+    //get all elements with class "textarea"
+    var timeBlockElements = $(".textarea");
+
+    //loop through textarea classes
+    for (var i = 0 ; i < timeBlockElements.length ; i++) {
+
+        //Get element i's ID as a string
+        var elementID = timeBlockElements[i].id;
+
+        //get element by ID
+        var manipID = document.getElementById(timeBlockElements[i].id)
+
+        //remove any old classes from element
+        $(timeBlockElements[i].id).removeClass(".present .past .future");
+
+        // apply new class if task is present/past/future
+        if (elementID < currentTime) {
+            $(manipID).addClass("past");
+        } else if (elementID > currentTime) {
+            $(manipID).addClass("future");
+        } else {
+            $(manipID).addClass("present");
+        }
+    }
+}
+
+// checkTime every 5 minutes
+setInterval(checkTime(), (1000 * 60) * 5);
+
+//
+
